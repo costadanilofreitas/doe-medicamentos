e7a59c0603ed27366852f96d4467a54736cf13dc
a2 <a2@localhost.localdomain>
2020-05-19 16:44:03 -0300

    Criacao de repositorio com projeto initializr

---
+++ src/main/java/com/doemedicamentos/DoeMedicamentosApplication.java
@@ -0,0 +1,13 @@
+package com.doemedicamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeMedicamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeMedicamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doemedicamentos/DoeMedicamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doemedicamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoeMedicamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
